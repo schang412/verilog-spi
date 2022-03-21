3b7ade00143b95fb309f63726a71ae457cfe5ef1
Spencer Chang <schang412@gmail.com>
2022-03-20 21:39:33 -0700

    rewrote spi master module to single file, fixed timing issues in accordance with analog devices timing diagrams

--- rtl/spi_master.sv
+++ rtl/spi_master.sv
@@ -1,73 +1,214 @@
+// Copyright (c) 2021-2022 Spencer Chang
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+`resetall
 `timescale 1ns / 1ps
-//NOTE: Enable is currently not tied to the tx and rx modules, I will decide on how to tie it in once I make tx changes
+`default_nettype none
+
 module spi_master #
 (
-    parameter AXIS_DATA_WIDTH = 8 // max = 64
+    parameter AXIS_DATA_WIDTH = 8,
+    parameter PRESCALE_WIDTH = 8
 )
 (
-    input  wire                  clk,
-    input  wire                  rst,
+    input  wire                         clk,
+    input  wire                         rst,
 
     /*
      * AXIS Input
      */
-    input  wire [AXIS_DATA_WIDTH-1:0] s_axis_tdata,
-    input  wire                       s_axis_tvalid,
-    output wire                       s_axis_tready,
+    input  wire [AXIS_DATA_WIDTH-1:0]   s_axis_tdata,
+    input  wire                         s_axis_tvalid,
+    output wire                         s_axis_tready,
 
     /*
      * AXIS Output
      */
-    output wire [AXIS_DATA_WIDTH-1:0] m_axis_tdata,
-    output wire                       m_axis_tvalid,
-    input  wire                       m_axis_tready,
+    output wire [AXIS_DATA_WIDTH-1:0]   m_axis_tdata,
+    output wire                         m_axis_tvalid,
+    input  wire                         m_axis_tready,
 
     /*
      * SPI
      */
-    output wire                  sclk,
-    output wire                  mosi,
-    input  wire                  miso,
+    output wire                         sclk,
+    output wire                         mosi,
+    input  wire                         miso,
 
     /*
      * Configuration
      */
-    input  wire [1:0]            spi_mode,
-    input  wire [7:0]            sclk_prescale,
-    input  wire [5:0]            spi_word_width,
+    input  wire [1:0]                       spi_mode,
+    input  wire [PRESCALE_WIDTH-1:0]        sclk_prescale,
+    input  wire [WORD_COUNTER_WIDTH-1:0]    spi_word_width,
 
     /*
      * Status
      */
-    output wire                  tx_busy,
-    output wire                  rx_busy,
-    output wire                  rx_overrun_error,
-    output wire                  bus_active
+    output wire                         rx_overrun_error,
+    output wire                         bus_active
 );
 
-reg sclk_buff = 0;
-reg sclk_en = 0; //Figure out when to toggle enable/what determines toggling
-reg [7:0] sclk_prescale_buff;
-reg [1:0] spi_mode_buff;
-reg [7:0] prescale_counter;
-reg [7:0] busy_counter = 0;
+localparam WORD_COUNTER_WIDTH = $clog2(AXIS_DATA_WIDTH) + 1;
 
+enum reg [1:0] {IDLE, TRANSFER} master_state = IDLE;
+
+// axis
+reg [AXIS_DATA_WIDTH-1:0] m_axis_tdata_reg = 0;
+reg m_axis_tvalid_reg = 0;
+assign m_axis_tdata = m_axis_tdata_reg;
+assign m_axis_tvalid = m_axis_tvalid_reg;
+assign s_axis_tready = (master_state == IDLE);
+
+// mosi
+reg mosi_reg = 0;
+reg miso_reg = 0;
+assign mosi = mosi_reg;
+
+// status
+reg rx_overrun_error_reg = 0;
 reg active = 0;
+assign bus_active = (master_state != IDLE);
+reg seen_posedge_sclk;
 
+// spi mode
+reg [1:0] spi_mode_buff;
+wire cpol, cpha;
 assign cpol = (spi_mode_buff === 2) | (spi_mode_buff === 3);
-assign cpha = (spi_mode_buff === 1) | (spi_mode_buff === 2);
+assign cpha = (spi_mode_buff === 1) | (spi_mode_buff === 3);
 
-assign bus_active = active;
+// spi word length
+reg [WORD_COUNTER_WIDTH-1:0] spi_word_width_buff = AXIS_DATA_WIDTH;
+reg [WORD_COUNTER_WIDTH-1:0] bit_out_cnt = 0;
+reg [WORD_COUNTER_WIDTH-1:0] bit_in_cnt = 0;
+
+// spi transaction contents
+reg [AXIS_DATA_WIDTH-1:0] data_o_reg = 0;
+reg [AXIS_DATA_WIDTH-1:0] data_i_reg = 0;
+
+always_ff @(posedge clk) begin : proc_spi_transaction
+    if (rst) begin
+        master_state <= IDLE;
+        mosi_reg <= 0;
+        data_i_reg <= 0;
+        rx_overrun_error_reg <= 0;
+        seen_posedge_sclk <= 0;
+    end else begin
+        miso_reg <= miso;
+
+        // handle an axis transaction
+        if (m_axis_tvalid && m_axis_tready) begin
+            m_axis_tvalid_reg <= 0;
+            rx_overrun_error_reg <= 0;
+        end
+
+        // state machine
+        case (master_state)
+            IDLE: begin
+                if (s_axis_tvalid && s_axis_tready) begin
+                    // configurable inputs
+                    spi_mode_buff <= spi_mode;
+                    spi_word_width_buff <= spi_word_width;
+                    sclk_prescale_buff <= sclk_prescale;
+
+                    data_o_reg <= s_axis_tdata;
+                    data_i_reg <= 0;
+
+                    seen_posedge_sclk <= 0;
+                    bit_in_cnt <= 0;
+                    bit_out_cnt <= 0;
+                    master_state <= TRANSFER;
+                end
+            end
+            TRANSFER: begin
+                // according to https://www.analog.com/en/analog-dialogue/articles/introduction-to-spi-interface.html
+                // the timing diagrams say that we will treat the rising edge as our first edge
+
+                // for cpha = 0, the rising edge is sampling, so we must prepare data on the wire
+                // before we see any clocks (as soon as we transition to the transfer state)
+                if ((~cpha) && (bit_out_cnt==0)) begin
+                    mosi_reg <= data_o_reg[spi_word_width_buff - 1];
+                    data_o_reg <= data_o_reg << 1;
+                    bit_out_cnt <= bit_out_cnt + 1;
+                end
+
+                if (sclk_rising_edge) begin
+                    seen_posedge_sclk <= 1;
+                end
+
+                // read stage
+                if (sclk_read_edge && (seen_posedge_sclk | sclk_rising_edge)) begin
+                    data_i_reg <= {data_i_reg[AXIS_DATA_WIDTH-2:0], miso_reg};
+                    bit_in_cnt <= bit_in_cnt + 1;
+                end
+
+                // write stage
+                if (sclk_write_edge && (seen_posedge_sclk | sclk_rising_edge)) begin
+                    mosi_reg <= data_o_reg[spi_word_width_buff - 1];
+                    data_o_reg <= data_o_reg << 1;
+                    bit_out_cnt <= bit_out_cnt + 1;
+                end
+
+                // the last step of spi transaction in any mode is a read
+                // so we will check for done by doing a spi read
+                if (bit_in_cnt == spi_word_width_buff) begin
+                    master_state <= IDLE;
+
+                    // write data back to axis
+                    m_axis_tvalid_reg <= 1;
+                    m_axis_tdata_reg <= data_i_reg;
+
+                    rx_overrun_error_reg <= m_axis_tvalid_reg;
+                end
+
+            end
+            default: master_state <= IDLE;
+        endcase
+    end
+end
 
 
 
+// generate sclk signal
+wire sclk_en;
+assign sclk_en = (master_state != IDLE);
+
+reg [PRESCALE_WIDTH-1:0] sclk_prescale_buff;
+reg [PRESCALE_WIDTH-1:0] prescale_counter;
+
+reg sclk_buff = 0;
 assign sclk = sclk_buff;
 
-enum reg [1:0] {IDLE, TX_WAIT, RX_WAIT} master_state = IDLE;
+// keep track of edges
+reg sclk_last;
+wire sclk_read_edge;
+wire sclk_write_edge;
+wire sclk_rising_edge;
+assign sclk_read_edge = (cpha) ? (sclk_last & ~sclk) : (~sclk_last & sclk);
+assign sclk_write_edge = (cpha) ? (~sclk_last & sclk) : (sclk_last & ~sclk);
+assign sclk_rising_edge = (~sclk_last & sclk);
 
-always_ff @(posedge clk) begin : proc_SCLK
+always_ff @(posedge clk) begin : proc_sclk_gen
     if (rst) begin
         prescale_counter <= 0;
+        sclk_last <= 0;
     end else begin
         if (sclk_en) begin
             prescale_counter <= prescale_counter + 1;
@@ -75,96 +216,16 @@ always_ff @(posedge clk) begin : proc_SCLK
                 sclk_buff <= !sclk_buff;
                 prescale_counter <= 0;
             end
-        end
-        else begin
+        end else begin
+            // let the sclk idle according to the polarity
             sclk_buff <= cpol;
-        end   
+            prescale_counter <= 0;
+        end
+        sclk_last <= sclk;
     end
 end
 
-always_ff @(negedge clk) begin // BUSY detection
-    if (rst) begin
-        busy_counter <= 0;
-        master_state <= IDLE;
-    end
-    case (master_state)
-        IDLE: begin // Waits to catch tx_busy == 1
-            sclk_prescale_buff <= sclk_prescale;
-            spi_mode_buff <= spi_mode;
-            if (tx_busy) begin
-                active <= 1;
-                master_state <= TX_WAIT;
-            end
-        end
-        TX_WAIT: begin //Waits one SCLK by counting busy_counter, then pulls sclk_en high
-            busy_counter <= busy_counter + 1;
-            if (busy_counter == sclk_prescale_buff - 1)
-                sclk_en <= 1; // NOTE: Since both SCLK generation and BUSY detection are both posedge sensitive, at least one CLK cycle will elapse before sclk generation begins.
-            if (rx_busy) begin
-                busy_counter <= 0;
-                master_state <= RX_WAIT;
-            end
-        end
-        RX_WAIT: begin //When !rx_busy gets detected, pulls sclk_en low and then counts for one SCLK
-            if (!rx_busy || !sclk_en) begin //NOTE: Could be problematic; if rx_busy does not remain low for at least one SCLK, state machine would get stuck in this state. One solution is to split it into two states, like IDLE and TX_WAIT
-                sclk_en <= 0; //NOTE: Same problem as stated in TX_WAIT
-                busy_counter <= busy_counter + 1;
-                if (busy_counter == sclk_prescale_buff - 1) begin 
-                    active <= 0;
-                    busy_counter <= 0;
-                end
-                if (!active) begin
-                    busy_counter <= busy_counter + 1;
-                    if (busy_counter == sclk_prescale_buff - 1) begin
-                        master_state <= IDLE; // Returns to IDLE to wait for the next tx_busy
-                        busy_counter <= 0;
-                    end
-                end
-            end
-        end
-    endcase
-end
-spi_rx #(
-    .AXIS_DATA_WIDTH(AXIS_DATA_WIDTH)
-) spi_rx_inst (
-    .clk(clk),
-    .rst(rst),
-
-    .m_axis_tdata  (m_axis_tdata),
-    .m_axis_tvalid (m_axis_tvalid),
-    .m_axis_tready (m_axis_tready),
-
-    .enable_capture(bus_active),
-
-    .sclk          (sclk_buff),
-    .rxd           (miso),
-
-
-    .spi_mode      (spi_mode),
-    .spi_word_width(spi_word_width),
-
-    .busy          (rx_busy),
-    .overrun_error (rx_overrun_error)
-);
-
-spi_tx #(
-    .AXIS_DATA_WIDTH(AXIS_DATA_WIDTH)
-) spi_tx_inst (
-    .sclk(sclk_buff),
-    .clk(clk),
-    .rst(rst),
-
-    .s_axis_tdata  (s_axis_tdata),
-    .s_axis_tvalid (s_axis_tvalid),
-    .s_axis_tready (s_axis_tready),
-
-    .txd           (mosi),
-    .tx_enable     (bus_active),
-
-    .spi_mode      (spi_mode),
-    .spi_word_width(spi_word_width),
-
-    .busy          (tx_busy)
-);
 
 endmodule : spi_master
+
+`resetall
--- tb/spi_master/test_spi_master.py
+++ tb/spi_master/test_spi_master.py
@@ -4,8 +4,6 @@ import logging
 import os
 import sys
 
-import configparser
-
 import cocotb_test.simulator
 import pytest
 
@@ -18,6 +16,7 @@ from cocotbext.axi import AxiStreamSource, AxiStreamSink, AxiStreamBus
 from cocotbext.spi import SpiSignals, SpiConfig
 from cocotbext.spi.devices.generic import SpiSlaveLoopback
 
+
 class TB(object):
     def __init__(self, dut, spi_mode, spi_word_width):
         self.dut = dut
@@ -28,19 +27,19 @@ class TB(object):
         cocotb.fork(Clock(dut.clk, 4, units="ns").start())
 
         spi_signals = SpiSignals(
-            sclk = dut.sclk,
-            mosi = dut.mosi,
-            miso = dut.miso,
-            cs   = dut.bus_active,
-            cs_active_low = False
+            sclk=dut.sclk,
+            mosi=dut.mosi,
+            miso=dut.miso,
+            cs=dut.bus_active,
+            cs_active_low=False
         )
 
         spi_config = SpiConfig(
-            word_width = spi_word_width,
-            cpol = bool(spi_mode in [2, 3]),
-            cpha = bool(spi_mode in [1, 2]),
-            msb_first = True,
-            frame_spacing_ns = 1
+            word_width=spi_word_width,
+            cpol=bool(spi_mode in [2, 3]),
+            cpha=bool(spi_mode in [1, 3]),
+            msb_first=True,
+            frame_spacing_ns=1
         )
 
         self.spi_loopback = SpiSlaveLoopback(spi_signals, spi_config)
@@ -48,27 +47,25 @@ class TB(object):
         self.source = AxiStreamSource(AxiStreamBus.from_prefix(dut, "s_axis"), dut.clk, dut.rst)
         self.sink = AxiStreamSink(AxiStreamBus.from_prefix(dut, "m_axis"), dut.clk, dut.rst)
 
-
     async def reset(self):
         self.dut.rst.setimmediatevalue(0)
         await RisingEdge(self.dut.clk)
         await RisingEdge(self.dut.clk)
-        self.dut.rst <= 1
+        self.dut.rst.value = 1
         await RisingEdge(self.dut.clk)
         await RisingEdge(self.dut.clk)
-        self.dut.rst <= 0
+        self.dut.rst.value = 0
         await RisingEdge(self.dut.clk)
         await RisingEdge(self.dut.clk)
-
 
 
 async def run_test(dut, payload_data=None, payload_lengths=None, spi_mode=None, spi_word_width=None):
     tb = TB(dut, spi_mode, spi_word_width)
     await tb.reset()
 
-    dut.sclk_prescale <= 4
-    dut.spi_word_width <= spi_word_width
-    dut.spi_mode <= spi_mode
+    dut.sclk_prescale.value = 4
+    dut.spi_word_width.value = spi_word_width
+    dut.spi_mode.value = spi_mode
 
     for test_data in [payload_data(x) for x in payload_lengths()]:
         await tb.source.write(test_data)
@@ -86,30 +83,34 @@ async def run_test(dut, payload_data=None, payload_lengths=None, spi_mode=None, 
     await RisingEdge(dut.clk)
     await RisingEdge(dut.clk)
 
+
 def size_list():
-    return list(range(1,16)) + [128]
+    return list(range(1, 16)) + [128]
+
 
 def incrementing_payload(length):
     return bytearray(itertools.islice(itertools.cycle(range(256)), length))
 
+
 def spi_word_width_list():
     return list([x for x in [8, 16, 32] if x <= int(os.environ["PARAM_AXIS_DATA_WIDTH"])])
+
 
 if cocotb.SIM_NAME:
     factory = TestFactory(run_test)
     factory.add_option("payload_lengths", [size_list])
     factory.add_option("payload_data", [incrementing_payload])
-    # factory.add_option("spi_mode", [0, 1, 2, 3])
-    factory.add_option("spi_mode", [1])
+    factory.add_option("spi_mode", [0, 1, 2, 3])
+    # factory.add_option("spi_mode", [1])
     factory.add_option("spi_word_width", spi_word_width_list())
     factory.generate_tests()
-
 
 
 tests_dir = os.path.dirname(__file__)
 rtl_dir = os.path.abspath(os.path.join(tests_dir, '../../rtl'))
 
-@pytest.mark.parametrize("axis_data_width", [8, 16, 32])
+
+@pytest.mark.parametrize("axis_data_width", [8, 16, 32, 64])
 def test_spi_tx(request, axis_data_width):
     dut = "spi_master"
     module = os.path.splitext(os.path.basename(__file__))[0]
@@ -117,23 +118,17 @@ def test_spi_tx(request, axis_data_width):
 
     verilog_files = [
         f"{dut}.sv",
-        "spi_rx.sv",
-        "spi_tx.sv"
     ]
     verilog_sources = [os.path.join(rtl_dir, x) for x in verilog_files]
 
-    # read the default parameters
-    config = configparser.ConfigParser()
-    config.read(os.path.join(tests_dir,"../parameters.ini"))
-    parameters = config._sections['default']
-
     # replace the parametrized parameters
+    parameters = {}
     parameters["AXIS_DATA_WIDTH"] = axis_data_width
 
     extra_env = {f'PARAM_{k}': str(v) for k, v in parameters.items()}
 
     sim_build = os.path.join(tests_dir, "sim_build",
-        request.node.name.replace('[', '-').replace(']', ''))
+                             request.node.name.replace('[', '-').replace(']', ''))
 
     cocotb_test.simulator.run(
         python_search=[tests_dir],
@@ -144,5 +139,3 @@ def test_spi_tx(request, axis_data_width):
         sim_build=sim_build,
         extra_env=extra_env,
     )
-
-
